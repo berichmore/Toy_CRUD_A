0dac6c3636335ca2bb1cb0b2197031810ddda09a
coding-faster <berichmore@naver.com>
2025-03-11 16:45:29 +0900

    Login form 작성

--- front/src/Components/member/Login.jsx
+++ front/src/Components/member/Login.jsx
@@ -3,11 +3,29 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <table>
+            <table className="table">
+                <tbody>
                 <tr>
-                    <th>아이디</th>
+                    <th className="col-3">아이디</th>
+                    <td>
+                        <input type="text" size="50px"/>
+                    </td>
                 </tr>
-            </table>
+                <tr>
+                    <th>비밀번호</th>
+                    <td>
+                        <input type="password" size="50px"/>
+                    </td>
+                </tr>
+                </tbody>
+            </table><br/>
+
+
+            <div className="my-1 d-flex justify-content-center">
+                <button className="btn btn-outline-secondary" >로그인</button>
+    
+            </div>
+
         </div>
     );
 };
