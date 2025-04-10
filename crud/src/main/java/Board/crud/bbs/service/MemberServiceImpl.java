8ad0cede4947a70dd98dadb6e4e71a52ff0c375d
coding-faster <berichmore@naver.com>
2025-04-10 22:47:36 +0900

    feat: Bcrypt 추가, 회원가입 구현중

--- crud/src/main/java/Board/crud/bbs/service/MemberServiceImpl.java
+++ crud/src/main/java/Board/crud/bbs/service/MemberServiceImpl.java
@@ -2,17 +2,24 @@ package board.crud.bbs.service;
 
 import board.crud.bbs.domain.MemberVO;
 import board.crud.bbs.mapper.MemberMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
     private final MemberMapper memberMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public MemberServiceImpl (MemberMapper memberMapper){
+    public MemberServiceImpl(MemberMapper memberMapper, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.memberMapper = memberMapper;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+
+
+
+    //로그인
     @Override
     public MemberVO login(String id, String pwd) {
         //먼저 아이디를 통해 회원 정보를 조회
@@ -24,5 +31,12 @@ public class MemberServiceImpl implements MemberService {
             //=--------------------------member로 리턴한다는게 뭐지?
         }
         return null;
+    }
+
+    //회원가입
+    @Override
+    public void join(MemberVO memeber) {
+
+
     }
 }
